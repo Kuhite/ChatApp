672bfaa7008be9377c8422479424484472a2ec18
Aayush Amar Kuhite <aayush.kuhite2@gmail.com>
2022-02-10 21:40:01 +0530

    Socket connection established

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+const path = require('path');
+const socketio = require('socket.io');
 
+
+const server = http.createServer(app);
+
+const io = new socketio.Server(server);
 
 const port = 3000;
 app.use(express.json());
 
-app.use('/',express.static('public'))
 
-app.get('/',(req,res) => {
-   
+const publicDirectory = path.join(__dirname,'public')
+app.use('/',express.static(publicDirectory))
+
+io.on('connection',() => {
+    console.log('A user connected!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Listening Successfully!")
 })
